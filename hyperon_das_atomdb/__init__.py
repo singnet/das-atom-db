06700256890d3f74d27aeda27763dadd0a9317fa
Andre Senna <andre.senna@gmail.com>
2023-12-04 13:32:37 -0300

    Expose NodeDoesNotExistException

--- hyperon_das_atomdb/__init__.py
+++ hyperon_das_atomdb/__init__.py
@@ -1,3 +1,4 @@
 from .database import UNORDERED_LINK_TYPES, WILDCARD, AtomDB
+from .exceptions import NodeDoesNotExistException
 
-__all__ = ['AtomDB', 'WILDCARD', 'UNORDERED_LINK_TYPES']
+__all__ = ['AtomDB', 'WILDCARD', 'UNORDERED_LINK_TYPES', 'NodeDoesNotExistException']
