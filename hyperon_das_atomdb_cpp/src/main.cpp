0b3a69a29690ef5cb2835a35f173963c70d964a1
Angelo Probst <angelo.probst@gmail.com>
2024-09-06 13:43:04 -0300

    wip

--- hyperon_das_atomdb_cpp/src/main.cpp
+++ hyperon_das_atomdb_cpp/src/main.cpp
@@ -11,8 +11,11 @@ using namespace atomdb;
 
 int main(int argc, char const* argv[]) {
     InMemoryDB db;
-    auto node_params = NodeParams(/*type*/ "Person", /*name*/ "John Doe");
-    node_params.custom_attributes.set("age", 42);
+    auto node_params = NodeParams(
+        /*type*/ "Person",
+        /*name*/ "John Doe",
+        /*custom_attributes*/ {{"gender", "male"}});
+    node_params.custom_attributes.set("age", 42);  // custom attribute can be set after initialization
     auto node = db.add_node(node_params);
     cout << "Node handle: " << node.handle << endl;
     cout << "Node name: " << node.name << endl;
@@ -25,28 +28,44 @@ int main(int argc, char const* argv[]) {
         "since": "2021-01-01",
         "location": "New York",
         "targets": [
-            { "type": "Person", "name": "Jane Doe" },            # a Person node
-            { "type": "Person", "name": "Samuel L. Jackson" },   # another Person node
+            { "type": "Person", "name": "Jane Doe" },            # a Person node as a target
+            { "type": "Person", "name": "Samuel L. Jackson" },   # another Person node as a target
             {
-                "type": "Fellowship",                            # a link as a target of Friendship
+                "type": "Fellowship",        # a Fellowship link as a target of Friendship
                 "targets": [
-                    { "type": "Person", "name": "Jane Doe" },    # a Person node as a target of Fellowship
-                    { "type": "Person", "name": "Michael Douglas" }  # another Person node as a target of Fellowship
+                    {
+                        "type": "Person",    # a Person node as a target of Fellowship
+                        "name": "Jane Doe"
+                    },
+                    {
+                        "type": "Person",    # another Person node as a target of Fellowship
+                        "name": "Michael Douglas"
+                    }
                 ]
             }
         ]
     }
     */
+    
+    // Initialize link
     auto link_params =
         LinkParams("Friendship",
                    /*custom_attributes*/ {{"since", "2021-01-01"}, {"location", "New York"}});
+    
+    // Adding 2 Person Nodes as targets
     link_params.add_target(NodeParams("Person", "Jane Doe"));
     link_params.add_target(NodeParams("Person", "Samuel L. Jackson"));
-    auto nested_link = LinkParams("Fellowship");
-    nested_link.add_target(NodeParams("Person", "Jane Doe"));
-    nested_link.add_target(NodeParams("Person", "Michael Douglas"));
-    link_params.add_target(nested_link);
+    
+    // Adding a Fellowship Link as a target of Friendship
+    auto link_as_target = LinkParams("Fellowship");
+    link_as_target.add_target(NodeParams("Person", "Jane Doe")); // target of Fellowship
+    link_as_target.add_target(NodeParams("Person", "Michael Douglas")); // target of Fellowship
+    link_params.add_target(link_as_target);
+    
+    // Adding the link to the database
     auto opt_link = db.add_link(link_params);
+    
+    // Printing the link details
     if (opt_link.has_value()) {
         auto link = opt_link.value();
         cout << "Link handle: " << link.handle << endl;
