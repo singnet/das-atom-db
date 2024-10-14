14673d7667031032bafd541fd4a702755916a8d5
Angelo Probst <angelo.probst@gmail.com>
2024-10-14 10:05:00 -0300

    dockerizing the build process

--- hyperon_das_atomdb_cpp/scripts/build_wheeler_docker_image.sh
+++ hyperon_das_atomdb_cpp/scripts/build_wheeler_docker_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source common.sh
+source ${PWD}/scripts/common.sh
 
-docker buildx build -t $WHEELER_CONTAINER_NAME --load -f $WHEELER_DOCKER_FILE .
+docker buildx build -t ${WHEELER_CONTAINER_NAME} \
+  --load -f ${WHEELER_DOCKER_FILE} .
 
--- hyperon_das_atomdb_cpp/scripts/run_wheeler.sh
+++ hyperon_das_atomdb_cpp/scripts/run_wheeler.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-source common.sh
+source ${PWD}/scripts/common.sh
 
 docker run --rm \
-    --name=$WHEELER_CONTAINER_NAME \
-    --volume .:/hyperon_das_atomdb_cpp \
+    -e _USER=$(id -u) \
+    -e _GROUP=$(id -g) \
+    -v $PWD:/hyperon_das_atomdb_cpp \
     --workdir /hyperon_das_atomdb_cpp \
+    --name=$WHEELER_CONTAINER_NAME \
     $WHEELER_CONTAINER_NAME
 
