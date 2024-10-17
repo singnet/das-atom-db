d3f92fa39076a94ec83ba6971983a5ddba27c5a0
Angelo Probst <angelo.probst@gmail.com>
2024-10-17 12:12:14 -0300

    everything under submodules, moving `FieldNames` from python to c++

--- hyperon_das_atomdb_cpp/src/constants.h
+++ hyperon_das_atomdb_cpp/src/constants.h
@@ -16,4 +16,20 @@ static const string TYPEDEF_MARK_HASH = ExpressionHasher::named_type_hash(":");
 
 enum FieldIndexType { BINARY_TREE = 0, TOKEN_INVERTED_LIST };
 
+struct FieldNames {
+    static const char* ID_HASH;
+    static const char* HANDLE;
+    static const char* COMPOSITE_TYPE;
+    static const char* COMPOSITE_TYPE_HASH;
+    static const char* NODE_NAME;
+    static const char* TYPE_NAME;
+    static const char* TYPE_NAME_HASH;
+    static const char* KEY_PREFIX;
+    static const char* KEYS;
+    static const char* IS_TOPLEVEL;
+    static const char* TARGETS;
+    static const char* TARGETS_DOCUMENTS;
+    static const char* CUSTOM_ATTRIBUTES;
+};
+
 }  // namespace atomdb
